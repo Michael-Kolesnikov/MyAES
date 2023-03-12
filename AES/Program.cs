510f507bb20eefe7c412d9ad32b9637dfe392fec
Michael-Kolesnikov <dark0pesto7@mail.ru>
2023-03-12 23:11:12 +0300

    Add initial project files

---
+++ AES/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ MyAEStests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MyAEStests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
