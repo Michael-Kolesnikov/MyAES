65641c32e27e35e59e5d99759d3a0877cad94c09
Michael-Kolesnikov <dark0pesto7@mail.ru>
2023-03-16 23:36:03 +0300

    fix gaps

--- AES/AES.cs
+++ AES/AES.cs
@@ -4,7 +4,7 @@ namespace MyAES
 {
     public class AES
     {
-        private FiniteField field = new FiniteField(2, 8,new int[] {1,0,0,0,1,1,1,0,1}); //x^8+x^4+x^3+x^2+1
+        private FiniteField field = new FiniteField(2, 8, new int[] { 1, 0, 0, 0, 1, 1, 1, 0, 1 }); //x^8+x^4+x^3+x^2+1
         private byte[] _key;
         private int _convertingNumber = 16;
         // must be the square of some number
@@ -22,18 +22,18 @@ namespace MyAES
 
         public byte[] Encode(byte[] msg)
         {
-            if(msg.Length % _fragmentationSize != 0) throw new ArgumentException("message size does not match the given parameters");
+            if (msg.Length % _fragmentationSize != 0) throw new ArgumentException("message size does not match the given parameters");
 
             var result = new List<byte>();
-            for(var i = 0; i < msg.Length; i += _fragmentationSize)
+            for (var i = 0; i < msg.Length; i += _fragmentationSize)
             {
-                byte[,] msgBlock = CreateMsgBlock(i,msg);
+                byte[,] msgBlock = CreateMsgBlock(i, msg);
                 byte[,] keyBlock = CreateKeyBlock(i);
-                for(var j = 0; j < _convertingNumber; j++)
+                for (var j = 0; j < _convertingNumber; j++)
                 {
                     //each byte is converted to an element of the final field, then its reverse is taken
                     msgBlock = GetInverse(msgBlock);
-                    msgBlock = GetXOR(msgBlock,keyBlock);
+                    msgBlock = GetXOR(msgBlock, keyBlock);
                     msgBlock = ShiftRows(msgBlock);
                     msgBlock = ShiftColomns(msgBlock);
                 }
@@ -48,7 +48,7 @@ namespace MyAES
             var matrixSize = (int)Math.Sqrt(_fragmentationSize);
             var block = new byte[matrixSize, matrixSize];
             for (var i = 0; i < matrixSize; i++)
-                for(var j = 0;j < matrixSize; j++)
+                for (var j = 0; j < matrixSize; j++)
                     block[i, j] = msg[startIndex++];
             return block;
         }
@@ -63,7 +63,7 @@ namespace MyAES
                     block[i, j] = _key[startIndex++];
             return block;
         }
-        
+
         private byte[,] GetInverse(byte[,] msgBlock)
         {
             for (var i = 0; i < msgBlock.GetLength(0); i++)
@@ -72,7 +72,7 @@ namespace MyAES
                         msgBlock[i, j] = field.GetFiniteFieldElement(new byte[] { msgBlock[i, j], 0, 0, 0 }).GetInverse().ConvertToByte()[0];
             return msgBlock;
         }
-        
+
         private static byte[,] GetXOR(byte[,] msgBlock, byte[,] keyBlock)
         {
             for (var i = 0; i < msgBlock.GetLength(0); i++)
@@ -81,7 +81,7 @@ namespace MyAES
                         msgBlock[i, j] = (byte)(msgBlock[i, j] ^ keyBlock[i, j]);
             return msgBlock;
         }
-        
+
         private static byte[,] ShiftRows(byte[,] msgBlock)
         {
             byte[,] tempArray = new byte[msgBlock.GetLength(0), msgBlock.GetLength(0)];
@@ -96,7 +96,7 @@ namespace MyAES
             }
             return tempArray;
         }
-        
+
         private static byte[,] ShiftColomns(byte[,] msgBlock)
         {
             byte[,] tempArray = new byte[msgBlock.GetLength(0), msgBlock.GetLength(0)];
@@ -110,7 +110,7 @@ namespace MyAES
             }
             return tempArray;
         }
-        
+
         public byte[] Decode(byte[] msg)
         {
             if (msg.Length % 16 != 0) throw new ArgumentException();
