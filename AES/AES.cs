3bc88e990cff28d0b6efd1646ff122d935d3dda3
Michael-Kolesnikov <dark0pesto7@mail.ru>
2023-03-23 13:52:57 +0300

    adding a liner and related changes

--- AES/AES.cs
+++ AES/AES.cs
@@ -4,75 +4,89 @@ namespace MyAES
 {
     public class AES
     {
-        private readonly FiniteField field = new(2, 8, new int[] { 1, 0, 0, 0, 1, 1, 1, 0, 1 }); //x^8+x^4+x^3+x^2+1
-        private readonly byte[] _key;
-        private readonly int _convertingNumber = 16;
+        private readonly FiniteField field = new(2, 8, new int[] { 1, 0, 0, 0, 1, 1, 1, 0, 1 }); // x^8+x^4+x^3+x^2+1
+        private readonly byte[] key;
+        private readonly int convertingNumber = 16;
+
         // must be the square of some number
-        private readonly int _fragmentationSize = 16;
+        private readonly int fragmentationSize = 16;
+
         public AES(byte[] key)
         {
-            if (key.Length != 256) throw new ArgumentException("key size does not match the given parameters");
-            _key = key;
+            if (key.Length != 256)
+            {
+                throw new ArgumentException("key size does not match the given parameters");
+            }
+
+            this.key = key;
         }
 
         public byte[] Encode(byte[] msg)
         {
-            if (msg.Length % _fragmentationSize != 0) throw new ArgumentException("message size does not match the given parameters");
+            if (msg.Length % this.fragmentationSize != 0)
+            {
+                throw new ArgumentException("message size does not match the given parameters");
+            }
 
             var result = new List<byte>();
-            for (var i = 0; i < msg.Length; i += _fragmentationSize)
+            for (var i = 0; i < msg.Length; i += this.fragmentationSize)
             {
                 byte[,] msgBlock = CreateMsgBlock(i, msg);
-                for (var j = 0; j < _convertingNumber; j++)
+                for (var j = 0; j < this.convertingNumber; j++)
                 {
-                    //each byte is converted to an element of the final field, then its reverse is taken
+                    // each byte is converted to an element of the final field, then its reverse is taken
                     byte[,] keyBlock = CreateKeyBlock(j * 16);
                     msgBlock = GetInverse(msgBlock);
                     msgBlock = GetXOR(msgBlock, keyBlock);
                     msgBlock = ShiftRows(msgBlock);
                     msgBlock = ShiftColumns(msgBlock);
                 }
+
                 byte[] msg2 = msgBlock.Cast<byte>().ToArray();
                 result.AddRange(msg2);
             }
+
             return result.ToArray();
         }
 
-        private byte[,] CreateMsgBlock(int startIndex, byte[] msg)
+        public byte[] Decode(byte[] msg)
         {
-            var matrixSize = (int)Math.Sqrt(_fragmentationSize);
-            var block = new byte[matrixSize, matrixSize];
-            for (var i = 0; i < matrixSize; i++)
-                for (var j = 0; j < matrixSize; j++)
-                    block[i, j] = msg[startIndex++];
-            return block;
-        }
+            if (msg.Length % 16 != 0)
+            {
+                throw new ArgumentException("msg length must divide by 16");
+            }
 
-        private byte[,] CreateKeyBlock(int startIndex)
-        {
-            startIndex %= _key.Length;
-            var matrixSize = (int)Math.Sqrt(_fragmentationSize);
-            var block = new byte[matrixSize, matrixSize];
-            for (var i = 0; i < matrixSize; i++)
-                for (var j = 0; j < matrixSize; j++)
-                    block[i, j] = _key[startIndex++];
-            return block;
-        }
+            var result = new List<byte>();
+            for (var i = 0; i < msg.Length; i += this.fragmentationSize)
+            {
+                byte[,] msgBlock = CreateMsgBlock(i, msg);
+                for (var j = 0; j < this.convertingNumber; j++)
+                {
+                    byte[,] keyBlock = CreateKeyBlock(240 - (j * 16));
 
-        private byte[,] GetInverse(byte[,] msgBlock)
-        {
-            for (var i = 0; i < msgBlock.GetLength(0); i++)
-                for (var j = 0; j < msgBlock.GetLength(1); j++)
-                    if (msgBlock[i, j] != 0)
-                        msgBlock[i, j] = field.GetFiniteFieldElement(new byte[] { msgBlock[i, j], 0, 0, 0 }).GetInverse().ConvertToByte()[0];
-            return msgBlock;
+                    msgBlock = ShiftColumns(msgBlock);
+                    msgBlock = ShiftRows(msgBlock);
+                    msgBlock = GetXOR(msgBlock, keyBlock);
+                    msgBlock = GetInverse(msgBlock);
+                }
+
+                byte[] msg2 = msgBlock.Cast<byte>().ToArray();
+                result.AddRange(msg2);
+            }
+
+            return result.ToArray();
         }
 
         private static byte[,] GetXOR(byte[,] msgBlock, byte[,] keyBlock)
         {
             for (var i = 0; i < msgBlock.GetLength(0); i++)
+            {
                 for (var j = 0; j < msgBlock.GetLength(1); j++)
+                {
                     msgBlock[i, j] = (byte)(msgBlock[i, j] ^ keyBlock[i, j]);
+                }
+            }
+
             return msgBlock;
         }
 
@@ -88,6 +102,7 @@ namespace MyAES
                     tempArray[newColumnIndex, j] = msgBlock[i, j];
                 }
             }
+
             return tempArray;
         }
 
@@ -102,29 +117,55 @@ namespace MyAES
                     tempArray[i, newColumnIndex] = msgBlock[i, j];
                 }
             }
+
             return tempArray;
         }
 
-        public byte[] Decode(byte[] msg)
+        private byte[,] GetInverse(byte[,] msgBlock)
         {
-            if (msg.Length % 16 != 0) throw new ArgumentException();
-            var result = new List<byte>();
-            for (var i = 0; i < msg.Length; i += _fragmentationSize)
+            for (var i = 0; i < msgBlock.GetLength(0); i++)
             {
-                byte[,] msgBlock = CreateMsgBlock(i, msg);
-                for (var j = 0; j < _convertingNumber; j++)
+                for (var j = 0; j < msgBlock.GetLength(1); j++)
                 {
-                    byte[,] keyBlock = CreateKeyBlock(240 - j * 16);
-
-                    msgBlock = ShiftColumns(msgBlock);
-                    msgBlock = ShiftRows(msgBlock);
-                    msgBlock = GetXOR(msgBlock, keyBlock);
-                    msgBlock = GetInverse(msgBlock);
+                    if (msgBlock[i, j] != 0)
+                    {
+                        msgBlock[i, j] = this.field.GetFiniteFieldElement(new byte[] { msgBlock[i, j], 0, 0, 0 }).GetInverse().ConvertToByte()[0];
+                    }
                 }
-                byte[] msg2 = msgBlock.Cast<byte>().ToArray();
-                result.AddRange(msg2);
             }
-            return result.ToArray();
+
+            return msgBlock;
+        }
+
+        private byte[,] CreateMsgBlock(int startIndex, byte[] msg)
+        {
+            var matrixSize = (int)Math.Sqrt(this.fragmentationSize);
+            var block = new byte[matrixSize, matrixSize];
+            for (var i = 0; i < matrixSize; i++)
+            {
+                for (var j = 0; j < matrixSize; j++)
+                {
+                    block[i, j] = msg[startIndex++];
+                }
+            }
+
+            return block;
+        }
+
+        private byte[,] CreateKeyBlock(int startIndex)
+        {
+            startIndex %= this.key.Length;
+            var matrixSize = (int)Math.Sqrt(this.fragmentationSize);
+            var block = new byte[matrixSize, matrixSize];
+            for (var i = 0; i < matrixSize; i++)
+            {
+                for (var j = 0; j < matrixSize; j++)
+                {
+                    block[i, j] = this.key[startIndex++];
+                }
+            }
+
+            return block;
         }
     }
 }
